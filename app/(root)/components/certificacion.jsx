9b4d9bb8560259c5ca649ea8c0b8d993961e142f
Reynoso <reynoso.geronimo@gmail.com>
2023-12-19 15:17:57 -0300

    certificacion

---
+++ app/(root)/components/certificacion.jsx
@@ -0,0 +1,34 @@
+import {AwardIcon } from "lucide-react";
+
+
+const Certificacion = () => {
+  return (
+    <section className="flex">
+      <div className="flex flex-col">
+        <AwardIcon/>
+        <h2>
+          Obtén la Certificación de mayor reconocimiento del mercado y Aval
+          Internacional.
+        </h2>
+        <p>
+          Accede a la doble Titulación, Certificado de Extensión Universitaria
+          IUYMCA, Certificado IdaClass con autenticación QR + Certificado de 4
+          especializaciones.
+        </p>
+      </div>
+      <div>
+        <h2>Sello de calidad en formación profesional</h2>
+        <p>
+          1ra Digital School de Latam con estándar de formación. 1ra Digital
+          School con normas de código y éticas profesional.
+        </p>
+        <p>Nuestros partners educativos nos avalan.</p>
+      </div>
+      <div>
+        ACA VAN LOS DIPLOMAS
+      </div>
+    </section>
+  );
+};
+
+export default Certificacion;
