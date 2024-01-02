966f063b9750e4aec51ad518d79061320e3be247
GeronimoReynoso <reynoso.geronimo@gmail.com>
2024-01-01 21:35:29 -0300

    wip paginas de blog y asesorate

---
+++ app/blog/page.jsx
@@ -0,0 +1,13 @@
+import { Construction } from 'lucide-react';
+
+
+const Page = () => {
+    return (
+        <div className="flex flex-col items-center">
+          <Construction size={72} className=" rounded-full" fill="#FFFF00"/>
+          <h1 className="text-3xl font-bold">Sitio bajo construccion</h1>
+        </div>
+      );
+}
+
+export default Page
