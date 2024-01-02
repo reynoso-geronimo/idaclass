b396159d24f3f0cf1917c6dd4685d778f9597a46
GeronimoReynoso <reynoso.geronimo@gmail.com>
2024-01-01 21:27:14 -0300

    cambio de config para limpiar error de webpack y sequelize

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['sequelize'],
+    }
+
+}
 
 module.exports = nextConfig
