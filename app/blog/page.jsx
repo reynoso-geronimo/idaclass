91ce0c430566d4aedfa0f9a55311fabb4ff7bff3
Juanmaanghilante <curne_juan@hotmail.com>
2024-01-08 17:40:42 -0300

    blog actualizado falta adaptar a responsive

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -53,24 +53,25 @@ const postCollection = [
     <main>
    
   
-        <div className="max-w-7xl mx-auto my-8 grid grid-cols-1 md:grid-cols-3 gap-12">
+        <div className="max-w-8xl p-8 mx-6 mx-auto my-8 grid grid-cols-1 md:grid-cols-3 gap-12 bg-red-200">
       {postCollection.map((item, index) => (
-        <div key={item.id} className={index === 0 ? "col-span-1 md:col-span-3 grid gap-4 md:gap-4vmin md:grid-cols-3 min-h-[320px] border-t relative" : "bg-white p-6 md:p-8 rounded-lg shadow-md flex items-center w-full md:w-auto"}>
+        <div key={item.id} className={index === 0 ? "p-8 bg-yellow-200 col-span-1 md:col-span-3 grid gap-4 md:gap-4vmin md:grid-cols-3 min-h-[320px] border-t relative" : "bg-white p-6 md:p-8 rounded-lg shadow-md flex items-center w-full md:w-auto"}>
           {index === 0 ? (
-            <div className=" md:flex h-full">
+            <div className="md:flex h-full w-auto">
                  <Image
                 src={item.image}
                 alt={item.title}
                 width={920}
                 height={1020}
-                className="rounded-lg"
+                className="rounded-lg   w-full h-auto md:w-auto md:h-full"
+            
               />
               <div className="relative flex-shrink-0 w-1/2">
                 {/* Aquí iría el contenido de tu imagen */}
              
             
               {/* Contenido del título, fecha, y párrafo a la derecha */}
-              <div className=" p-6 md:w-96">
+              <div className=" p-6 md:w-96  ">
                 <h2 className="text-2xl font-bold mb-2">{item.title}</h2>
                 <p className="text-gray-500 mb-2">Fecha de publicación</p>
                 <p className="text-gray-700 mb-2">{item.paragraph}</p>
