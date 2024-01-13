6844887925bbb9860ecc6cdeefd93940d96a6f2c
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-13 11:34:59 -0300

    fecha de publicacion blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,60 +1,13 @@
 import Image from "next/image";
 import { getBlogPostFromDb } from "../actions";
 
-const postCollection = [
-  {
-    id: 1,
-    title: "Salud nutricion y tecnologia",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 1.",
-  },
-  {
-    id: 2,
-    title: "Las claves de una rutina exitosa",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 2.",
-  },
-  {
-    id: 3,
-    title: "Como desarrollar tu marca personal en el mundo fitness",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 3.",
-  },
-  {
-    id: 4,
-    title: "Como desarrollar tu marca personal en el mundo fitness",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 3.",
-  },
-  {
-    id: 5,
-    title: "Como desarrollar tu marca personal en el mundo fitness",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 3.",
-  },
-  {
-    id: 6,
-    title: "Como desarrollar tu marca personal en el mundo fitness",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 3.",
-  },
-  {
-    id: 7,
-    title: "Como desarrollar tu marca personal en el mundo fitness",
-    image: "/assets/funcionalcrosshiit.webp",
-    paragraph: "Descripción del Item 3.",
-  },
-];
-
 const Page = async () => {
   const blogPost = await getBlogPostFromDb();
-  console.log(blogPost);
   console.log(blogPost);
   return (
     <main className="flex flex-col items-center">
       <div className="max-w-7xl p-8 my-8 grid grid-cols-1 md:grid-cols-3 items-center	gap-12">
-        {
-        blogPost.map((item, index) => (
+        {blogPost.map((item, index) => (
           <div
             key={item.id}
             className={`bg-white flex mx-auto p-6 rounded-lg shadow-md max-w-[420px] min-h-[500px] ${
@@ -79,7 +32,13 @@ const Page = async () => {
               />
               <div className={` ${index === 0 ? "" : ""}`}>
                 <h2 className="text-xl font-semibold ">{item.titulo}</h2>
-                <p className="text-gray-500">Fecha de publicación</p>
+                <p className="text-gray-500">
+                  {new Date(item.created_at).toLocaleDateString(undefined, {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </p>
                 <p className="text-gray-700">{item.introduccion}</p>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-6 md:mt-0">
                   Leer más
