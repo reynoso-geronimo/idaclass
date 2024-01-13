6c9b95fad720c710b1d60dd2f295a78ecc784b8b
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-13 11:56:53 -0300

    code clean

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -3,7 +3,7 @@ import { getBlogPostFromDb } from "../actions";
 
 const Page = async () => {
   const blogPost = await getBlogPostFromDb();
-  console.log(blogPost);
+
   return (
     <main className="flex flex-col items-center">
       <div className="max-w-7xl p-8 my-8 grid grid-cols-1 md:grid-cols-3 items-center	gap-12">
@@ -11,14 +11,13 @@ const Page = async () => {
           <div
             key={item.id}
             className={`bg-white p-6 rounded-lg shadow-md max-w-[420px] min-h-[500px] ${
-              index === 0
-                ? "md:col-span-3 md:max-w-full md:w-full  md:min-h-[10px] "
-                : ""
+              index === 0 &&
+              "md:col-span-3 md:max-w-full md:w-full  md:min-h-[10px] "
             }`}
           >
             <div
               className={`w-full flex flex-col justify-start gap-y-6 h-full  ${
-                index === 0 ? "md:flex-row md:gap-x-12 md:items-center" : ""
+                index === 0 && "md:flex-row md:gap-x-12 md:items-center"
               }`}
             >
               <Image
@@ -27,10 +26,10 @@ const Page = async () => {
                 width={420}
                 height={420}
                 className={`rounded-lg object-cover  ${
-                  index === 0 ? "md:w-2/3" : ""
+                  index === 0 && "md:w-2/3"
                 }`}
               />
-              <div className={` ${index === 0 ? "" : ""}`}>
+              <div>
                 <h2 className="text-xl font-semibold ">{item.titulo}</h2>
                 <p className="text-gray-500">
                   {new Date(item.created_at).toLocaleDateString(undefined, {
@@ -40,8 +39,8 @@ const Page = async () => {
                   })}
                 </p>
                 <p
-                  className={`text-gray-700 line-clamp-6 ${
-                    index === 0 ? "md:line-clamp-[8]" : ""
+                  className={`text-gray-700 line-clamp-[8] sm:line-clamp-6 ${
+                    index === 0 && "md:line-clamp-[8]"
                   }`}
                 >
                   {item.introduccion}
