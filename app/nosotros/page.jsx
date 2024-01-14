d684d7aa66b03a2161845a3d062b8b9f2ba018be
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-14 12:10:56 -0300

    rediseño de la pagina nosotros

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -10,6 +10,24 @@ import {
 import { Button } from "@/components/ui/button";
 import Partners from "@/components/partners";
 
+const articles = [
+  {
+    title: "Nuestra Historia",
+    paragraph:
+      "IDACLASS, es fundada en Octubre 2021 en Florida, EE.UU., por Julio Alghilante ,Argentino residente en Buenos Aires. Cabe destacar que Julio, inició este negocio en forma presencial en Argentina hace más de 20 años, llegando a tener 40.000 alumnos distribuidos principalmente en Argentina, Uruguay y Bolivia. Producto de la pandemia, el negocio se vió fuertemente afectado y fue entonces que se fundó una nueva compañía en USA con el objetivo de una revolución Digital para la empresa misma buscando innovar en tecnología ,servicio y expandirnos asi a todo Latino America.",
+  },
+  {
+    title: "Nuestra Presente",
+    paragraph:
+      "Esta nueva compañía, Edtech para el deporte y la actividad física, propone virtualizar el negocio y vender online a todo Hispanoamérica. Vendemos cursos de formación y especialización para la industria del deporte , actividad física y fitness. Formamos a entrenadores y especialistas que luego pueden emprender sus propios negocios independientes y/o trabajar luego en instituciones deportivas y gimnasios.",
+  },
+  {
+    title: "Nuestro Futuro",
+    paragraph:
+      "En el horizonte de IdaClass LLC, vislumbramos un futuro vibrante y prometedor, marcado por nuestra incansable dedicación a transformar la educación y la formación en el ámbito del deporte y la actividad física en Hispanoamérica.",
+  },
+];
+
 const Page = () => {
   return (
     <main className="w-full flex flex-col items-center">
@@ -25,7 +43,7 @@ const Page = () => {
           className="shrink-1"
         />
       </section>
-      <section className="text-white bg-gray-900 w-full flex flex-col items-center p-4">
+      <section className="text-white bg-gray-900 w-full flex flex-col items-center px-4 py-12">
         <div className="text-center max-w-3xl">
           <h2 className="text-2xl md:text-3xl font-bold"> Sobre Nosotros</h2>
           <p className="text-lg md:text-xl">
@@ -36,80 +54,30 @@ const Page = () => {
           </p>
         </div>
 
-        <div className="flex flex-col justify-center items-center w-full max-w-3xl gap-10 pt-12">
-          <article className="flex items-center justify-center mr-auto">
-            <Image
-              src={`/assets/HeroLG.webp`}
-              alt="hero image"
-              height={200}
-              width={200}
-              className="shrink-1"
-            />
-            <div className="w-56">
-              <h3 className="text-lg md:text-xl font-semibold">
-                Nuestra Historia
-              </h3>
-              <p className="text-pretty md:text-lg">
-                IDACLASS, es fundada en Octubre 2021 en Florida, EE.UU., por
-                Julio Alghilante ,Argentino residente en Buenos Aires. Cabe
-                destacar que Julio, inició este negocio en forma presencial en
-                Argentina hace más de 20 años, llegando a tener 40.000 alumnos
-                distribuidos principalmente en Argentina, Uruguay y Bolivia.
-                Producto de la pandemia, el negocio se vió fuertemente afectado
-                y fue entonces que se fundó una nueva compañía en USA con el
-                objetivo de una revolución Digital para la empresa misma
-                buscando innovar en tecnología ,servicio y expandirnos asi a
-                todo Latino America.
-              </p>
-            </div>
-          </article>
-          <article className="flex flex-row-reverse items-center justify-center ml-auto">
-            <Image
-              src={`/assets/HeroLG.webp`}
-              alt="hero image"
-              height={200}
-              width={200}
-              className="shrink-1"
-            />
-            <div className="w-56">
-              <h3 className="text-lg md:text-xl font-semibold">
-                Nuestra Presente
-              </h3>
-              <p className="text-pretty md:text-lg">
-                Esta nueva compañía, Edtech para el deporte y la actividad
-                física, propone virtualizar el negocio y vender online a todo
-                Hispanoamérica. Vendemos cursos de formación y especialización
-                para la industria del deporte , actividad física y fitness.
-                Formamos a entrenadores y especialistas que luego pueden
-                emprender sus propios negocios independientes y/o trabajar luego
-                en instituciones deportivas y gimnasios.
-              </p>
-            </div>
-          </article>
-
-          <article className="flex items-center justify-center mr-auto">
-            <Image
-              src={`/assets/HeroLG.webp`}
-              alt="hero image"
-              height={200}
-              width={200}
-              className="shrink-1"
-            />
-            <div className="w-56">
-              <h3 className="text-lg md:text-xl font-semibold">
-                Nuestro Futuro
-              </h3>
-              <p className="text-pretty md:text-lg">
-                En el horizonte de IdaClass LLC, vislumbramos un futuro vibrante
-                y prometedor, marcado por nuestra incansable dedicación a
-                transformar la educación y la formación en el ámbito del deporte
-                y la actividad física en Hispanoamérica.
-              </p>
-            </div>
-          </article>
+        <div className="flex flex-col justify-center items-center w-full max-w-4xl gap-y-20 p-6">
+          {articles.map((article, index) => (
+            <article
+              className="flex flex-col md:flex-row even:md:flex-row-reverse items-center justify-center "
+              key={index}
+            >
+              <Image
+                src={`/funcional.webp`}
+                alt="hero image"
+                height={200}
+                width={200}
+                className="w-full md:w-1/2 rounded-lg"
+              />
+              <div className="md:w-1/2">
+                <h3 className="text-lg md:text-xl font-semibold">
+                  {article.title}
+                </h3>
+                <p className="text-pretty md:text-lg">{article.paragraph}</p>
+              </div>
+            </article>
+          ))}
         </div>
       </section>
-      <section className="w-full max-w-3xl flex flex-col items-center bg-slate-220 gap-4 py-12">
+      <section className="w-full max-w-4xl flex flex-col items-center bg-slate-220 gap-4 pñoñx-4 py-12">
         <h2 className="text-2xl font-bold text-idaclass">Nuestros Valores</h2>
         <div className="grid grid-cols-2  gap-2 md:gap-4 lg:grid-cols-4 justify-items-center content-center	">
           <Card className="w-54">
@@ -169,7 +137,7 @@ const Page = () => {
           </Card>
         </div>
       </section>
-     <Partners/>
+      <Partners />
       <section className="flex flex-col text-center sm:ml-auto sm:mr-24 my-12">
         <h4 className="text-lg font-semibold">Suscribete al newsletter</h4>
         <p className="px-4">Ultimas noticias relacionadas al mundo fitness</p>
