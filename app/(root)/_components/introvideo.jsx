3e67d40bf10160a48bd3127716e2021624dcb2db
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-23 22:35:39 -0300

    cambios de estilos

--- app/(root)/_components/introvideo.jsx
+++ app/(root)/_components/introvideo.jsx
@@ -10,7 +10,7 @@ const Introvideo = () => {
   return (
     <div className="flex w-full justify-center ">
       <Dialog>
-        <DialogTrigger className="text-idaclass px-2 rounded-2xl flex text-2xl font-bold items-center gap-2 shadow-[0_0px_40px_20px_rgba(255,255,255,0.6)] bg-gray-300">
+        <DialogTrigger className="text-idaclass px-2 rounded-2xl flex text-lg sm:text-2xl font-bold items-center gap-2 shadow-[0_0px_40px_20px_rgba(255,255,255,0.6)] bg-gray-300">
           <YoutubeIcon size={40}/> Conoce Idaclass
         </DialogTrigger>
         <DialogContent className="pt-12 bg-zinc-800 bg-opacity-95 border-none">
