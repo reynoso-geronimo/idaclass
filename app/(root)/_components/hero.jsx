7fefebcf879a8c8694ea578aa79e3d7fc09bee94
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-23 22:17:44 -0300

    probando cambios

--- app/(root)/_components/hero.jsx
+++ app/(root)/_components/hero.jsx
@@ -4,16 +4,7 @@ import { Separator } from "../../../components/ui/separator";
 import { Button } from "../../../components/ui/button";
 import Link from "next/link";
 import Introvideo from "./introvideo";
-import Autoplay from "embla-carousel-autoplay";
-
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import Image from "next/image";
+import Partners from "./ui/partners";
 
 const OPTIONS = { loop: true };
 const images = [
@@ -47,40 +38,24 @@ const images = [
     alt: "personal trainer",
   },
 ];
-const partners = [
-  {
-    src: "/Aletic.png",
-    alt: "logo",
-  },
-  {
-    src: "/GimnasioST.jpg",
-    alt: "logo",
-  },
-  {
-    src: "/Graciela.jpeg",
-    alt: "logo",
-  },
-  {
-    src: "/gymPeru.png",
-    alt: "logo",
-  },
-  {
-    src: "/LogoB2Peru.png",
-    alt: "logo",
-  },
-  {
-    src: "/NeurofitRosario.jpg",
-    alt: "logo",
-  },
-];
+
 const Hero = () => {
   return (
     <div className=" bg-gradient-to-br from-gray-50 via-gray-100 to-gray-300 pt-16">
       <div className="lg:hidden">
         <h2 className="text-3xl md:text-5xl font-extrabold max-w-[340px] md:max-w-[600px]  mb-4 mx-4 text-indigo-950 tracking-tighter animate-in slide-in-from-right fade-in-0 duration-1000">
-          Unete a la revolucion educativa Sport Fitness
+          Unete a la <span className="text-blue-700">revolucion educativa</span> Sport Fitness
         </h2>
-        <EmblaCarousel slides={images} options={OPTIONS} />
+        {/* <EmblaCarousel slides={images} options={OPTIONS} /> */}
+        <video
+              autoPlay
+              muted
+              loop
+              className="-z-10 object-cover"
+              playsInline
+            >
+              <source src="/videos/idaclass.mp4" type="video/mp4" />
+            </video>
         <h2 className="mt-12 mx-8 text-center text-md text-blue-700 font-medium leading-6 tracking-[4px] ">
           Transforma tu pasion, despierta tu potencial, conviertete en un
           profesional con nuestros cursos.
@@ -122,12 +97,12 @@ const Hero = () => {
               autoPlay
               muted
               loop
-              className="-z-10 object-cover hidden sm:block"
+              className="-z-10 object-cover hidden sm:block rounded-md"
               playsInline
             >
-              <source src="/videos/video(2160p).mp4" type="video/mp4" />
+              <source src="/videos/idaclass.mp4" type="video/mp4" />
             </video>
-            <div className="relative bottom-[70px]  lg:bottom-[10%] translate-x-[50%] right-[50%] h-0">
+            <div className="relative bottom-[70px] lg:bottom-[10%] translate-x-[50%] right-[50%] h-0">
               <Introvideo />
             </div>
           </div>
@@ -136,43 +111,7 @@ const Hero = () => {
       <div className="text-xl font-bold text-center container">
         Idaclass junto a empresas destacadas del Sport Fitness de LATAM.
       </div>
-      <div className="container flex justify-center items-center h-40">
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          className="mx-4"
-          plugins={[
-            Autoplay({
-              delay: 2000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {partners.map((partner, index) => (
-              <CarouselItem
-                key={index}
-                className="basis-1/3 sm:basis-1/6 md:basis-1/6"
-              >
-                <div>
-                  <div className="flex aspect-square items-center justify-center px-2 relative">
-                    <Image
-                      src={`/partners${partner.src}`}
-                      width={120}
-                      height={120}
-                      alt="logo"
-                      className="grayscale rounded-xl cover"
-                    ></Image>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+      <Partners />
       <Separator className=" bg-gray-200" />
     </div>
   );
