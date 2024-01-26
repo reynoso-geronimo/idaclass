e40339445a73f579da20fbcdc499064387a6ba88
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-26 19:34:25 -0300

    carrusel de bandera

--- app/(root)/_components/banderas.jsx
+++ app/(root)/_components/banderas.jsx
@@ -1,21 +1,51 @@
+"use client"
 import Image from "next/image";
-const paises = ["ar", "py", "bo", "uy", "cl", "pe", "ec", "co"];
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+const paises = ["ar", "py", "bo", "uy", "cl", "pe", "ec", "co","mx","cr","ni","sv"];
 
 const Banderas = () => {
   return (
-    <section  className="flex container justify-center mb-12">
-      {paises.map(pais => (
-        <div key={pais}>
-          <Image
-            src={`https://flagcdn.com/${pais}.svg`}
-            width={100}
-            height={100}
-            alt={pais}
-            className="shadow-2xl"
-          ></Image>
-          
-        </div>
-      ))}
+    <section className=" flex justify-center items-center h-40 container">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+          duration:6000,
+          watchDrag:false
+        }}
+        plugins={[
+          Autoplay({
+            delay: 0,
+          }),
+        ]}
+        className="mx-4 max-w-5xl container"
+       
+        
+      >
+        <CarouselContent>
+          {paises.map(pais => (
+            <CarouselItem key={pais} className="basis-1/7 ">
+              <div className="relative h-24 w-24 border-2 rounded-full">
+                <Image
+                  src={`https://flagcdn.com/h240/${pais}.webp`}
+                  fill
+                  alt={pais}
+                  className=" rounded-full "
+                  style={{
+                    objectFit: 'fill', // cover, contain, none
+                  }}
+                ></Image>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 };
