7e9557fdc642d4807d5de274b6c3a9f4ea6c0d0d
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-28 10:06:03 -0300

    se mejoro la funcionalidad de soluciones

--- app/(root)/_components/soluciones.jsx
+++ app/(root)/_components/soluciones.jsx
@@ -1,7 +1,11 @@
+'use client'
 import { ChevronDown } from "lucide-react";
 import { soluciones } from "../../../lib/constants.js";
+import { useState } from "react";
 
 const Soluciones = () => {
+  const [activo, setActivo] = useState()
+  
   return (
     <section
       className=" h-[800px] bg-zinc-800 text-white bg-no-repeat bg-[50%] bg-[length:_1400px] transform scale-x-[-1]"
@@ -15,23 +19,24 @@ const Soluciones = () => {
 
           {soluciones.map((solucion, index) => (
             <div
-              className="sm:flex items-center gap-4 group min-h-[100px] md:max-h-[100px] max-w-2xl relative"
+              className="sm:flex items-center gap-4 min-h-[100px] md:max-h-[100px] max-w-2xl relative"
               key={index + solucion.titulo}
+              onMouseEnter={()=>setActivo(index)}
             >
-              <span className="flex items-center font-extrabold text-5xl min-w-[70px] tracking-tighter font-mono scale-y-125 group-hover:drop-shadow-[0_0_10px_rgba(100,100,255,1)] transition-all duration-1000 group-hover:text-sky-300 hover:animate-none">
+              <span className={`flex items-center font-extrabold text-5xl min-w-[70px] tracking-tighter font-mono scale-y-125 ${activo==index&&` drop-shadow-[0_0_10px_rgba(100,100,255,1)] transition-all duration-1000  text-sky-300`} `}>
                 0{index + 1}
-                <span className="animate-bounce ml-4 group-hover:opacity-0 transition-all duration-1000 sm:hidden">
+                <span className={`animate-bounce ml-4 transition-all duration-1000 sm:hidden ${activo==index&&`opacity-0`}`}>
                   <ChevronDown />
                 </span>
               </span>
               <div>
-                <h5 className="flex items-center font-bold sm:text-2xl group-hover:text-base transition-all duration-1000">
+                <h5 className={`flex items-center font-bold sm:text-2xl `}>
                   {solucion.titulo}
-                  <span className="ml-4 py-12 group-hover:opacity-0 transition-all duration-1000 hidden sm:block sm:absolute -right-4">
+                  <span className={`ml-4 py-12  transition-all duration-1000 hidden sm:block sm:absolute -right-4  ${activo==index&&`opacity-0`}`}>
                     <ChevronDown size={32} />
                   </span>
                 </h5>
-                <p className="max-h-0 overflow-hidden transition-all duration-1000 ease-in-out group-hover:max-h-40 sm:text-md">
+                <p className={`max-h-0 overflow-hidden transition-all duration-1000 ease-in-out sm:text-md  ${activo==index&&`max-h-40`}`}>
                   {solucion.texto}
                 </p>
               </div>
