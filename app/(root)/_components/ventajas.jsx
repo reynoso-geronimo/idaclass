fef8d9d617d4e5cb240af66a8ce712f96fbfd42d
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-01-28 12:15:59 -0300

    probando cambios

--- app/(root)/_components/ventajas.jsx
+++ app/(root)/_components/ventajas.jsx
@@ -41,7 +41,7 @@ const Ventajas = () => {
   });
   const [ref2, inView2, entry2] = useInView({
     triggerOnce: true,
-    rootMargin: "-300px 0px",
+    rootMargin: "-200px 0px",
   });
   const [ref3, inView3, entry3] = useInView({
     triggerOnce: true,
