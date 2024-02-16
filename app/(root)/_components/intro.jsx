ae1d49551e437b661149f6388645701bd043db5c
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-15 23:51:37 -0300

    una forma de crear un svg

--- app/(root)/_components/intro.jsx
+++ app/(root)/_components/intro.jsx
@@ -1,4 +1,4 @@
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, Dot, Minus } from "lucide-react";
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -11,7 +11,7 @@ import {
 
 const Intro = () => {
   return (
-    <section className="container lg:px-0 w-full mx-auto bg-slate-900 text-white py-8 gap-12 flex flex-col lg:flex-row items-center justify-end ">
+    <section className="container lg:px-0 bg-slate-900 text-white py-8 gap-12 flex flex-col lg:flex-row items-center justify-end relative">
       <div className="w-full lg:w-auto text-center lg:text-left">
         <h2 className="font-semibold text-3xl lg:max-w-sm">
           Ofrecemos la mejor solución probada para tu futuro{" "}
@@ -27,6 +27,26 @@ const Intro = () => {
           Somos la llave para transformar tu pasion <br /> en una carrera
           exitosa en tiempo record.
         </p>
+        <div className="flex text-idaclass lg:absolute bottom-6 -translate-x-[27px] lg:left-[33px]">
+        <Minus className=" rotate-45 scale-x-[300%]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] max-sm:hidden" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px]" />
+        <Minus className="scale-x-[200%] translate-x-[7px] translate-y-[16px] z-10" />
+        <Dot className="translate-x-[0px] translate-y-[16px] scale-150 text-idaclass drop-shadow-[0_0_3px_rgba(100,200,255,01)]" />
+      </div>
       </div>
       <div className="w-full lg:w-1/2">
         <Carousel className="w-[100%]">
