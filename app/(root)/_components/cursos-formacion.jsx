da6da5546b641a30a729f32b3acf527a6b5514eb
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-18 10:57:39 -0300

    fix sucio

--- app/(root)/_components/cursos-formacion.jsx
+++ app/(root)/_components/cursos-formacion.jsx
@@ -43,7 +43,7 @@ const CursosFormacion = () => {
 
   const OPTIONS = { align: "start" };
   return (
-    <section className="my-12">
+    <section className="my-12 ">
       <TituloSeccion className={`mb-0`}>Cursos de formacion</TituloSeccion>
       <h2 className="text-center font-bold text-xl text-gray-700 ">
         Sé parte de la nueva generacion de entrenadores
@@ -51,7 +51,7 @@ const CursosFormacion = () => {
       <h2 className="text-center font-bold text-xl text-gray-700 mb-4">
         Forja tu camino en el mundo del Sport fitness
       </h2>
-      <div className="max-w-2xl mx-auto lg:max-w-full">
+      <div className="max-w-2xl mx-auto container lg:max-w-full">
         <EmblaCarousel
           options={OPTIONS}
           slides={cursos.map((curso, index) => (
