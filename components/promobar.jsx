15181efb1fbc63766a73d4d17f786368e0260516
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-19 18:37:10 -0300

    fix

--- components/promobar.jsx
+++ components/promobar.jsx
@@ -7,7 +7,7 @@ const Promobar = () => {
 
   return (
     <div className={`py-2 bg-primary text-white text-center ${Close && "hidden"} flex justify-end`}>
-      <p className="w-full text-center">¡Comprá ahora y pagá en 10 cuotas sin interés con tarjeta de crédito! &#128293;</p>
+      <p className="ml-2 -full text-center">¡Comprá ahora y pagá en 10 cuotas sin interés con tarjeta de crédito! &#128293;</p>
       <X className="mr-2 lg:mr-12" onClick={setClose} />
     </div>
   );
