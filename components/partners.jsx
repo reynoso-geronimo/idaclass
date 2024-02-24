1de67a4e3524e2bbe6da82723c798dadcaab8cb5
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-24 01:11:32 -0300

    modulo de partners

--- components/partners.jsx
+++ components/partners.jsx
@@ -1,29 +1,115 @@
-const Partners = () => {
-    const partnerLogos = [
-      "LOGO1",
-      "LOGO2",
-      "LOGO3",
-      "LOGO4",
-      "LOGO5",
-      "LOGO6",
-    ];
+"use client";
+
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+
+import TituloSeccion from "./ui/titulo-seccion";
+
+
+const partners = [
+  {
+    src: "/Aletic.png",
+    alt: "logo",
+  },
+  {
+    src: "/GimnasioST.jpg",
+    alt: "logo",
+  },
+  {
+    src: "/Graciela.jpeg",
+    alt: "logo",
+  },
+  {
+    src: "/gymPeru.png",
+    alt: "logo",
+  },
+
+  {
+    src: "/NeurofitRosario.jpg",
+    alt: "logo",
+  },
+  {
+    src: "/Aletic.png",
+    alt: "logo",
+  },
+  {
+    src: "/GimnasioST.jpg",
+    alt: "logo",
+  },
+  {
+    src: "/Graciela.jpeg",
+    alt: "logo",
+  },
+  {
+    src: "/gymPeru.png",
+    alt: "logo",
+  },
   
-    return (
-      <section className="w-full flex flex-col items-center space-y-4 bg-gray-300 px-4 min-h-72 py-12">
-        <h2 className="text-center text-2xl font-bold mb-6">Nos avalan</h2>
-        <div className="w-full max-w-4xl flex flex-wrap justify-evenly items-center gap-6">
-          {partnerLogos.map((logoText, index) => (
-            <div
-              className="bg-gray-700 w-28 h-20 flex justify-center items-center text-white font-extrabold text-xl shadow-2xl"
-              key={index}
-            >
-              {logoText}
-            </div>
-          ))}
-        </div>
-      </section>
-    );
-  };
-  
-  export default Partners;
-  
+  {
+    src: "/NeurofitRosario.jpg",
+    alt: "logo",
+  },
+];
+const PartnersEducativos = () => {
+  return (
+    <section className="container lg:px-0 bg-black text-white py-8 flex flex-col lg:flex-row items-center justify-end relative overflow-hidden">
+      <div className="w-full lg:w-auto ">
+        <TituloSeccion className={`text-idaclass4 lg:text-left lg:pr-4`}>
+          Nuestros Partners<br/> en educacion
+        </TituloSeccion>
+
+        {/* <div className="-translate-x-[27px] lg:absolute bottom-10 left-7 w-full">
+          <Image
+            src={linea}
+            alt="linea"
+            className="absolute -translate-x-[8%] -translate-y-[55%] scale-[150%] "
+          />
+        </div> */}
+      </div>
+      <div className="w-full lg:w-2/3">
+        <Carousel
+          className="w-[100%]"
+          opts={{
+            align: "start",
+            loop: true,
+            watchDrag: false,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+        >
+          <CarouselContent className="-ml-1">
+            {partners.map((partner, index) => (
+              <CarouselItem key={index} className="pl-1 basis-[37%] sm:basis-[20%]">
+                <div className="p-1">
+                  <div className="rounded-xl bg-none">
+                    <div className="flex aspect-square items-center justify-center p-6 relative bg-white rounded-xl">
+                      <Image
+                        src={`/partners${partner.src}`}
+                        fill
+                        className="object-contain object-center rounded-xl"
+                        alt={partner.src}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious className="left-2" />
+          <CarouselNext className="right-2" /> */}
+        </Carousel>
+      </div>
+    </section>
+  );
+};
+
+export default PartnersEducativos;
