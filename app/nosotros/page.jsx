0579bc1a13a4caaaf528744f00a7f8f11dc0aaf9
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-24 21:06:01 -0300

    fix

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -27,45 +27,49 @@ const Page = () => {
           className="shrink-1"
         />
       </section>
-  
-
-        <section className="text-white bg-gray-900 w-full flex flex-col items-center px-4 pb-12 ">
+      <section className=" bg-gray-900 w-full overflow-hidden">
         <TracingBeam className={""}>
-          <div className="text-center max-w-3xl py-12">
-            <h2 className="text-2xl md:text-3xl font-bold"> Sobre Nosotros</h2>
-            <p className="text-lg md:text-xl">
-              Lddddorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reprehenderit ad magnam libero corporis repudiandae fuga rerum
-              nisi qui, iusto odit quas eius. Quis, quam ipsam natus adipisci
-              vel assumenda nulla?
-            </p>
-          </div>
+          <div className="text-white flex flex-col items-center px-4 my-12 ">
+            <div className="text-center">
+              <h2 className="text-2xl md:text-3xl font-bold">
+                {" "}
+                Sobre Nosotros
+              </h2>
+              <p className="text-lg md:text-xl">
+                Lddddorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit ad magnam libero corporis repudiandae fuga rerum
+                nisi qui, iusto odit quas eius. Quis, quam ipsam natus adipisci
+                vel assumenda nulla?
+              </p>
+            </div>
 
-          <div className="flex flex-col justify-center items-center w-full max-w-4xl gap-y-20 p-6">
-            {sobreNosotros.map((article, index) => (
-              <article
-                className="flex flex-col md:flex-row even:md:flex-row-reverse items-center justify-center gap-4"
-                key={index}
-              >
-                <Image
-                  src={`/funcional.webp`}
-                  alt="hero image"
-                  height={200}
-                  width={200}
-                  className="w-full md:w-1/2 rounded-lg"
-                />
-                <div className="md:w-1/2">
-                  <h3 className="text-lg md:text-xl font-semibold">
-                    {article.title}
-                  </h3>
-                  <p className="text-pretty md:text-lg">{article.paragraph}</p>
-                </div>
-              </article>
-            ))}
+            <div className="flex flex-col justify-center items-center w-full gap-y-20 p-6">
+              {sobreNosotros.map((article, index) => (
+                <article
+                  className="flex flex-col md:flex-row even:md:flex-row-reverse items-center justify-center gap-4"
+                  key={index}
+                >
+                  <Image
+                    src={`/funcional.webp`}
+                    alt="hero image"
+                    height={200}
+                    width={200}
+                    className="w-full md:w-1/2 rounded-lg"
+                  />
+                  <div className="md:w-1/2">
+                    <h3 className="text-lg md:text-xl font-semibold">
+                      {article.title}
+                    </h3>
+                    <p className="text-pretty md:text-lg">
+                      {article.paragraph}
+                    </p>
+                  </div>
+                </article>
+              ))}
+            </div>
           </div>
-          </TracingBeam>
-        </section>
-     
+        </TracingBeam>
+      </section>
       <section className="container flex flex-col items-center bg-slate-220 gap-4 py-12">
         <h2 className="text-2xl font-bold text-idaclass">Nuestros Valores</h2>
         <div className="grid grid-cols-2  gap-2 md:gap-4 lg:grid-cols-4 justify-items-center content-center	">
