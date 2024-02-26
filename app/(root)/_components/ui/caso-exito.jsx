83c446a87653b6dfaba5ae4005f1226c07459626
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-02-26 20:49:43 -0300

    mas grande en pantallas grandes

--- app/(root)/_components/ui/caso-exito.jsx
+++ app/(root)/_components/ui/caso-exito.jsx
@@ -12,23 +12,26 @@ const CasoExito = ({
   pais,
 }) => {
   return (
-    <div className="bg-gray-800 container px-4 mx-auto py-2 text-white flex flex-col justify-around rounded-3xl max-w-60 aspect-square">
+    <div className="bg-gray-800 container px-4 mx-auto py-2 text-white flex flex-col justify-around rounded-3xl max-w-60 lg:max-w-72 aspect-square">
       <div className="flex gap-1">
         <Quote className="transform -rotate-180" />
         <Avatar>
-          <AvatarImage src={`https://flagcdn.com/${pais ? pais : "ar"}.svg`} alt={`${pais ? pais : "argentina"}`}/>
+          <AvatarImage
+            src={`https://flagcdn.com/${pais ? pais : "ar"}.svg`}
+            alt={`${pais ? pais : "argentina"}`}
+          />
           <AvatarFallback className="text-black">PP</AvatarFallback>
         </Avatar>
         <div>
-          <h3 className="text-bold flex items-center gap-1 text-sm">
+          <h3 className="text-bold flex items-center gap-1 text-sm lg:text-base">
             {nombre ? nombre : "Perez Con Pan"}
           </h3>
-          <p className="text-xs font-light text-gray-200">
-            {carrera ? carrera : "Preparador Fisico"}
+          <p className="text-xs lg:text-sm font-light text-gray-200">
+            {carrera ? carrera : "Preparador Físico"}
           </p>
         </div>
       </div>
-      <p className="text-xs italic">
+      <p className="text-xs italic lg:text-sm">
         {experiencia
           ? experiencia
           : "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
