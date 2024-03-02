00a9420be8a47375c6731f6fdcdb745a45dd4138
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-02 14:56:02 -0300

    cambio de color blanco

--- app/(root)/_components/beca.jsx
+++ app/(root)/_components/beca.jsx
@@ -8,12 +8,12 @@ const Beca = () => {
     <section className="w-full container mt-12">
       <div className="flex flex-col lg:flex-row gap-4 items-stretch justify-center">
      <Asesorate/>
-        <div className="bg-black w-full text-white text-center p-8 overflow-hidden rounded-3xl">
+        <div className="w-full text-center p-8 overflow-hidden rounded-3xl border-2 border-black">
           <TituloSeccion className={"mb-4"}>
             Obtén tu Beca<span className="inline text-idaclass4">Class</span> 40%
           </TituloSeccion>
           <h4 className="text-pretty md:text-lg">Impulsa tu futuro</h4>
-          <p className="my-6 relative z-10 opacity-100 text-white">
+          <p className="my-6 relative z-10 opacity-100 ">
             &quot;Nuestra misión es abrir las puertas a la educación en
             Latinoamérica Facilitamos el acceso a la educación de calidad de una
             forma simple y accesible. Accede ahora a la BecaClass del 40% y
@@ -26,7 +26,7 @@ const Beca = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 width="22.769"
                 height="14.821"
-                className="fill-idaclass scale-[1700%] opacity-50"
+                className="fill-idaclass scale-[1700%] opacity-30"
               >
                 <path d="M10.212 12.007 7.645 9.414h10v-4H7.62l2.585-2.586L7.377 0 0 7.378l7.37 7.443 2.842-2.814z" />
               </svg>
@@ -34,7 +34,7 @@ const Beca = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 width="22.769"
                 height="14.821"
-                className="fill-idaclass4 scale-[600%] -translate-x-6 -translate-y-[36px] "
+                className="fill-idaclass4 scale-[600%] -translate-x-6 -translate-y-[36px] opacity-70"
               >
                 <path d="M10.212 12.007 7.645 9.414h10v-4H7.62l2.585-2.586L7.377 0 0 7.378l7.37 7.443 2.842-2.814z" />
               </svg>
