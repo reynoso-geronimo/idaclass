497a99244cb42d4e20d02af0423c7e2bb780acbb
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-02 14:31:43 -0300

    re hacer

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function SignIn() {
   return (
     <main className="flex flex-col items-center relative">
-      <div className="flex flex-col border-2 border-idaclass3 absolute top-12 sm:right-52 space-y-4 bg-slate-900 p-6 rounded-lg shadow-lg z-10">
+      <div className="flex flex-col border-2 border-idaclass33 space-y-4 bg-slate-900 p-6 rounded-lg shadow-lg z-10">
         <Button
           onClick={() =>
             signIn("google", { callbackUrl: window.location.origin })
@@ -25,24 +25,7 @@ export default function SignIn() {
           />
           Ingresar con Google
         </Button>
-        {/* <Button
-          onClick={() =>
-            signIn("instagram", { callbackUrl: window.location.origin })
-          }
-          className="w-48 bg-slate-600 rounded-lg"
-          disabled
-        >
-          <Image
-            src={InstagramIcon}
-            alt="instagram"
-            style={{
-              width: "auto",
-              height: "100%",
-              marginRight: "0.2rem",
-            }}
-          />
-          Ingresar con Instagram
-        </Button> */}
+        
       </div>
     </main>
   );
