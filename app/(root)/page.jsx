7e059fb7b646ee75556fc41518b0c47c6cf25970
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-15 18:46:44 -0300

    optimizacion de codigo y placeholder

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -12,8 +12,28 @@ import EquipoProfesional from "./_components/equipo-profesional";
 import Beca from "./_components/beca";
 import EmpresasPartners from "./_components/empresasParteners";
 import dynamic from "next/dynamic";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-const DynamicModalidad=dynamic(()=>import("./_components/modalidad"),{ssr:false})
+const DynamicModalidad = dynamic(() => import("./_components/modalidad"), {
+  ssr: false,
+  loading: () => (
+    <div className="text-white pt-[15%] lg:pt-12 container text-center bg-black h-[400px]">
+      <h3 className="text-idaclass3 text-4xl font-bold">CURSOS ON DEMAND</h3>
+
+      <ul className="list-inside list-disc my-2">
+        <li>Estudia a tu ritmo </li>
+        <li>Expertos internacionales </li>
+        <li>Elije tu area de especializacion</li>
+      </ul>
+      <Link href={"/cursos"}>
+        <Button variants="lg" className="rounded-lg mt-4">
+          Ver todos los cursos
+        </Button>
+      </Link>
+    </div>
+  ),
+});
 export default function Home() {
   return (
     <main className="overflow-hidden">
