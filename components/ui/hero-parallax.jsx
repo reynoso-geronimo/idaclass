f3c3b553e5215f2e6938fa33bdeb3b1acaa79a96
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-16 17:31:24 -0300

    mejoras

--- components/ui/hero-parallax.jsx
+++ components/ui/hero-parallax.jsx
@@ -52,7 +52,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[2500px] md:h-[2800px] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[2500px] md:h-[2500px] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
@@ -64,7 +64,7 @@ export const HeroParallax = ({
         }}
         className=""
       >
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-14">
           {firstRow.map(product => (
             <ProductCard
               product={product}
@@ -73,7 +73,7 @@ export const HeroParallax = ({
             />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row  mb-20 space-x-20 ">
+        <motion.div className="flex flex-row  mb-14 space-x-20 ">
           {secondRow.map(product => (
             <ProductCard
               product={product}
@@ -119,12 +119,12 @@ export const ProductCard = ({ product, translate }) => {
         y: -20,
       }}
       key={product.title}
-      className="group/product h-96 w-[30rem] relative flex-shrink-0"
+      className="group/product h-96 w-[12rem] lg:w-[24rem] relative flex-shrink-0"
     >
-      <Link
+      {/* <Link
         href={product.link}
         className="block group-hover/product:shadow-2xl "
-      >
+      > */}
         <Image
           src={product.thumbnail}
           height="600"
@@ -132,10 +132,10 @@ export const ProductCard = ({ product, translate }) => {
           className="object-contain object-center-top absolute h-full w-full inset-0"
           alt={product.title}
         />
-      </Link>
-      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
+      {/* </Link> */}
+      {/* <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div> */}
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
-        {product.title}
+        {/* {product.title} */}
       </h2>
     </motion.div>
   );
