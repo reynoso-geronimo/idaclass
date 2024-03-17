46c4c32dff7c044a681b2055624358b28e0f9f2c
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-17 13:54:14 -0300

    cambio

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -39,7 +39,7 @@ export const valores = [
 const Page = () => {
   return (
     <main className="w-full flex flex-col items-center">
-      <section className="container">
+      <section className="max-w-full">
         {" "}
         <HeroParallaxNosotros />
       </section>
