c035b098c17d33c5195f5fa86906a65e91802282
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-03-31 20:26:44 -0300

    contenido del curso

--- app/curso-formacion/[nombre]/page.jsx
+++ app/curso-formacion/[nombre]/page.jsx
@@ -1,14 +1,8 @@
-import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
+
 import { Button } from "@/components/ui/button";
 
 import { ChevronRight, MapPin, Plus } from "lucide-react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+
 import { Separator } from "@/components/ui/separator";
 import TarjetaModalidad from "./_components/tarjeta-modalidad";
 
@@ -22,47 +16,12 @@ import linea from "../../../public/assets/Línea img 4.png";
 import CertificacionCursoFormacion from "./_components/certificacionCursoFormacion";
 import Link from "next/link";
 import Beca from "@/components/beca";
-import LineaConectar from "@/components/ui/lineaConectar";
 import CursoFormacionHeader from "./_components/cursoFormacionHeader";
 import CursoFormacionAcerca from "./_components/cursoFormacionAcerca";
 import CursoFormacionObjetivos from "./_components/cursoFormacionObjetivos";
-const contenido = [
-  {
-    titulo: "Habilidades para el éxito profesional",
-    unidades: [
-      {
-        titulo: "Marketing y ventas",
-        contenido: [
-          "Proceso de comunicación",
-          "Comunicación asertiva",
-          "Empatía para comunicar",
-        ],
-      },
-      {
-        titulo: "Comunicación asertiva",
-        contenido: [
-          "El mensaje verbal: estructura",
-          "Copywriting y Storytelling: diferencias y consejos",
-        ],
-      },
-      {
-        titulo: "Liderazgo",
-        contenido: [
-          "8 pasos para armar tu propia historia",
-          "El Viaje del Héroe",
-          "Método Self-Us-Now",
-        ],
-      },
-      {
-        titulo: "Herramientas digitales",
-        contenido: [
-          "Ejercicio de FODA para tu marca personal",
-          "El autorretrato para una comunicación efectiva",
-        ],
-      },
-    ],
-  },
-];
+import Image from "next/image";
+import CursoFormacionContenidoCurso from "./_components/cursoFormacionContendidoCurso";
+
 
 const CursoPage = async ({ params }) => {
   const nombreParseado = params.nombre.replace(/%20/g, " ");
@@ -109,73 +68,7 @@ const CursoPage = async ({ params }) => {
       <Separator className="my-6" />
       <CertificacionCursoFormacion />
       <Separator className="my-6" />
-      <section>
-        <h2 className="text-2xl lg:text-4xl font-bold my-4 text-center">
-          Contenido del curso
-        </h2>
-        <p className="text-center my-4">Subtitulo</p>
-
-        <div className="container flex flex-col items-center md:items-start md:flex-row gap-4">
-          <Accordion type="single" collapsible className="w-full md:w-1/2">
-            {JSON.parse(curso.modulos).map((modulo, index) => (
-              <AccordionItem value={`item-${index + 1}`} key={index}>
-                <AccordionTrigger className="font-bold text-sm md:text-xl  text-left px-4 ">
-                  {modulo.titulo}
-                </AccordionTrigger>
-                <AccordionContent className="font-semibold text-sm p-4  bg-gray-200">
-                  {modulo.contenido.map((item, index) => (
-                    <p key={index} className="flex items-center mb-4">
-                      <ChevronRight className="text-black shrink-0" />
-                      {item}
-                    </p>
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-
-          <Plus size={72} className="md:mt-[12%]" />
-
-          <div className="mx-auto w-full md:w-1/3 font-medium flex flex-col justify-between border  rounded-xl p-4">
-            <Badge className="text-lg text-center font-semibold m-2 justify-center bg-indigo-900  outline outline-2 outline-offset-4">
-              Bono Extra
-            </Badge>
-            <Image
-              src={`/assets/SkillClass.jpg`}
-              alt="skillclass"
-              className="mx-auto"
-              height={400}
-              width={400}
-            />
-            {contenido.map((item, index) => (
-              <div key={item + index} className="my-4">
-                {/* <h3 className="text-center font-bold">{item.titulo}</h3> */}
-                <Accordion type="single" collapsible className="w-full ">
-                  {item.unidades.map((unidad, i) => (
-                    <AccordionItem
-                      value={unidad}
-                      key={unidad.titulo + i}
-                      className="m-0"
-                    >
-                      <AccordionTrigger className="font-bold text-sm md:text-xl text-left px-4 py-1 ">
-                        {unidad.titulo}
-                      </AccordionTrigger>
-                      <AccordionContent className="font-semibold text-sm p-4">
-                        {unidad.contenido.map((item, j) => (
-                          <p key={j} className="flex items-center mb-4">
-                            <ChevronRight className="text-black shrink-0" />
-                            {item}
-                          </p>
-                        ))}
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
-                </Accordion>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
+      <CursoFormacionContenidoCurso modulos={curso.modulos} />
       <Separator className="my-6" />
 
       <section className="flex flex-col md:flex-row justify-center gap-4 container">
