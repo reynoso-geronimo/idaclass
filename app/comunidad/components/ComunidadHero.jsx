31126b2a51b3d30015c9c883bd994941800acdc4
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-08 20:20:17 -0300

    cambio colores

--- app/comunidad/components/ComunidadHero.jsx
+++ app/comunidad/components/ComunidadHero.jsx
@@ -10,8 +10,8 @@ const ComunidadHero = () => {
         <div className="w-full flex flex-col max-lg:items-center max-lg:text-center">
           <TituloSeccion className={`lg:text-left mb-2 p-0 ml-0`}>
             ¡Sumate a la <br className="max-lg:hidden" />
-            Comunidad <span className="text-idaclass3">Class</span>
-            <span className="text-idaclass4">era!</span>
+            Comunidad <span className="text-idaclass4">Class</span>
+            <span className="text-idaclass3">era!</span>
           </TituloSeccion>
           <h3 className="text-base font-semibold mb-4 text-idaclass4 italic">
             &quot;Conectado para transformar el ecosistema Sport Fitness&quot;
