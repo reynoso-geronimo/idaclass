52b028ec9715266d68d6e11fe42a8b7c39422b41
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-14 23:34:02 -0300

    align start loop true

--- app/cursos/_components/cursosOnDemandCategorias.jsx
+++ app/cursos/_components/cursosOnDemandCategorias.jsx
@@ -1,4 +1,3 @@
-
 import TituloSeccion from "@/components/ui/titulo-seccion";
 
 import {
@@ -10,25 +9,31 @@ import {
 } from "@/components/ui/carousel";
 import CursoOnDemandCard from "./curso-on-demand-card";
 
-const CursosOnDemandCategorias = ({ categoria,cursos }) => {
+const CursosOnDemandCategorias = ({ categoria, cursos }) => {
   return (
     <section className="">
       <TituloSeccion>
         Categoria <span className="text-idaclass4">{categoria}</span>
       </TituloSeccion>
-      <Carousel className="container">
+      <Carousel
+        className="container"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
         <CarouselContent className="-ml-1">
           {cursos.map((curso, index) => (
             <CarouselItem
               key={index}
               className="pl-1 md:basis-1/2 lg:basis-1/3"
             >
-              <CursoOnDemandCard curso={curso}/>
+              <CursoOnDemandCard curso={curso} />
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className={"left-2"}/>
-        <CarouselNext className={"right-2"}/>
+        <CarouselPrevious className={"left-2"} />
+        <CarouselNext className={"right-2"} />
       </Carousel>
     </section>
   );
