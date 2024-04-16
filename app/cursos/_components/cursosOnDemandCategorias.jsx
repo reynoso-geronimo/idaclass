0ffaa8ac3414aa4449d480afd0119c9c4646029b
Reynoso <reynoso.geronimo@gmail.com>
2024-04-16 10:08:38 -0300

    probando fix

--- app/cursos/_components/cursosOnDemandCategorias.jsx
+++ app/cursos/_components/cursosOnDemandCategorias.jsx
@@ -13,9 +13,11 @@ const CursosOnDemandCategorias = ({ categoriasOnDemand, cursos }) => {
       <nav className="container sticky top-[64px] z-50 flex items-center justify-evenly bg-gray-100 h-[60px] w-full gap-8">
         {categoriasOnDemand.map((categoriaOnDemand, index) => (
           <Link
-            onClick={setTimeout(() => {
-              setActivo(index)
-            }, 500)}
+            onClick={()=>{
+              setTimeout(() => {
+                setActivo(index)
+              }, 500)
+            }}
             key={index}
             href={`#${categoriaOnDemand}`}
             className={`${activo===index&&"bg-idaclass4 text-white"} rounded-2xl font-bold border-idaclass4 text-idaclass4 border-2 py-2 text-center w-full`}
