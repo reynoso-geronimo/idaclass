be819e438920aab3c742dbb94ccf315fac08bcb8
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-16 20:03:47 -0300

    textos

--- app/curso-formacion/[nombre]/_components/cursoFormacionObjetivos.jsx
+++ app/curso-formacion/[nombre]/_components/cursoFormacionObjetivos.jsx
@@ -1,14 +1,24 @@
 import { Check } from "lucide-react";
 import Image from "next/image";
 import React from "react";
-import bgWeb from '../../../../public/cursosFormacion/objetivosBgWeb.png'
-import bgMobile from '../../../../public/cursosFormacion/objetivosBgMobile.png'
+import bgWeb from "../../../../public/cursosFormacion/objetivosBgWeb.png";
+import bgMobile from "../../../../public/cursosFormacion/objetivosBgMobile.png";
 const CursoFormacionObjetivos = () => {
   return (
     <section className="">
       <div className="container flex flex-col lg:flex-row lg:pt-8 relative">
-        <Image src={bgWeb} fill className="w-full h-full object-cover object-left -z-10 max-lg:hidden" alt="background" />
-        <Image src={bgMobile} fill className="w-full h-full object-cover object-left -z-10 lg:hidden" alt="background" />
+        <Image
+          src={bgWeb}
+          fill
+          className="w-full h-full object-cover object-left -z-10 max-lg:hidden"
+          alt="background"
+        />
+        <Image
+          src={bgMobile}
+          fill
+          className="w-full h-full object-cover object-left -z-10 lg:hidden"
+          alt="background"
+        />
         <Image
           src={"/cursosFormacion/objetivos.png"}
           height={1000}
@@ -24,9 +34,14 @@ const CursoFormacionObjetivos = () => {
           >
             Objetivos <br className="lg:hidden" /> del Curso
           </h3>
-          <Objetivo />
-          <Objetivo />
-          <Objetivo />
+          <Objetivo
+            titulo="Conocimiento Técnico - Teórico:"
+            texto={
+              "Buscamos que los alumnos adquieran un profundo conocimiento respaldado por evidencia científica en el ámbito de la preparación física deportiva, así como dominar las últimas tácticas y tecnologías aplicables al entrenamiento."
+            }
+          />
+          <Objetivo titulo="Experiencia Práctica:" texto={"Nos enfocamos en dotar a los alumnos con habilidades prácticas sólidas, permitiéndoles aplicar de manera efectiva los conocimientos teóricos adquiridos en situaciones reales de entrenamiento, tanto individuales como de equipos."}/>
+          <Objetivo titulo="Habilidades actuales:" texto={" Que el egresado incorpore conocimiento especializado en marketing personal, ventas, que le permita atraer y retener clientes como asi tambien soluciones concretas  para promocionar sus servicios con éxito, expandir su red de clientes y alcanzar el éxito financiero en el ámbito de la preparación física deportiva."}/>
         </article>
         <article className="container text-white pb-8 px-4">
           <h3
