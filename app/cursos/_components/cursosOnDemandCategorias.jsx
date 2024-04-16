80d680c1467a703485ac004c17ba7fb4ff2a6529
Reynoso <reynoso.geronimo@gmail.com>
2024-04-16 09:20:45 -0300

    se agrego onclick set activo por las dudas

--- app/cursos/_components/cursosOnDemandCategorias.jsx
+++ app/cursos/_components/cursosOnDemandCategorias.jsx
@@ -13,6 +13,7 @@ const CursosOnDemandCategorias = ({ categoriasOnDemand, cursos }) => {
       <nav className="container sticky top-[64px] z-50 flex items-center justify-evenly bg-gray-100 h-[60px] w-full gap-8">
         {categoriasOnDemand.map((categoriaOnDemand, index) => (
           <Link
+            onClick={()=>setActivo(index)}
             key={index}
             href={`#${categoriaOnDemand}`}
             className={`${activo===index&&"bg-idaclass4 text-white"} rounded-2xl font-bold border-idaclass4 text-idaclass4 border-2 py-2 text-center w-full`}
