5734e20c2a589520d991582ba56fd41e7f4aa386
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-21 17:17:56 -0300

    cambios de estilo

--- app/comunidad/components/ComunidadIntro.jsx
+++ app/comunidad/components/ComunidadIntro.jsx
@@ -5,12 +5,12 @@ import React from "react";
 const ComunidadIntro = () => {
   return (
     <section className="container flex max-lg:flex-col justify-around gap-12 my-12">
-      <div className=" rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6 ">
+      <div className="lg:w-1/3 rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6">
         <Image
           src={`/comunidad/Imagen Card 1.png`}
           alt=""
           fill
-          className="object-cover -z-10"
+          className="object-cover object-top -z-10"
         />
         <h3 className="relative top-0 text-2xl text-idaclass font-bold leading-none">
           Ida<span className="text-idaclass4">Class</span>
@@ -31,12 +31,12 @@ const ComunidadIntro = () => {
         </Button>
       </div>
 
-      <div className=" rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6 ">
+      <div className="lg:w-1/3 rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6">
         <Image
           src={`/comunidad/Imagen Card 2.png`}
           alt=""
           fill
-          className="object-cover -z-10"
+          className="object-cover object-top -z-10"
         />
         <h3 className="relative top-0 text-2xl text-idaclass font-bold leading-none">
           Ida<span className="text-idaclass4">Class</span>
@@ -57,12 +57,12 @@ const ComunidadIntro = () => {
         </Button>
       </div>
 
-      <div className=" rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6 ">
+      <div className="lg:w-1/3 rounded-3xl relative text-white overflow-hidden flex flex-col justify-between p-8 pt-6">
         <Image
           src={`/comunidad/Imagen Card 3.png`}
           alt=""
           fill
-          className="object-cover -z-10"
+          className="object-cover object-top -z-10"
         />
         <h3 className="relative top-0 text-2xl text-idaclass font-bold leading-none">
           Ida<span className="text-idaclass4">Class</span>
