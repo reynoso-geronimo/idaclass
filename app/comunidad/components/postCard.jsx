157e00352a6194aa0f0bed511177dc2ae70791f5
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-20 23:46:01 -0300

    tags

--- app/comunidad/components/postCard.jsx
+++ app/comunidad/components/postCard.jsx
@@ -1,10 +1,12 @@
+import { Badge } from "@/components/ui/badge";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
-    <article className=" rounded-b-2xl rounded-t-3xl overflow-hidden max-w-[520px] mx-auto">
+    <article className=" rounded-b-2xl rounded-t-3xl overflow-hidden max-w-[520px] mx-auto relative">
+     {post.tag&& <Badge className={`absolute top-4 -left-4 pl-6 bg-orange-500`}>{post.tag}</Badge>}
       <Image
         src={"/blog/" + post.foto}
         width={520}
@@ -13,7 +15,7 @@ const PostCard = ({ post }) => {
         className="bg-black aspect-video mx-auto text-white"
       />
       <div className="px-4 h-36 flex flex-col justify-between py-4 bg-gray-100">
-        <h5 className="leading-5 font-semibold line-clamp-[3]">
+        <h5 className="leading-5 font-medium line-clamp-[3]">
           {post.titulo}
         </h5>
         <Link
