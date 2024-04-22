af3f660091854a4a55cb79b89f47a377b6f58c85
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-04-22 07:55:35 -0300

    se agrego drop shadow

--- app/comunidad/components/ComunidadRegistro.jsx
+++ app/comunidad/components/ComunidadRegistro.jsx
@@ -5,7 +5,7 @@ import RegisterForm from "@/app/signin/_components/registerForm";
 
 const ComunidadRegistro = () => {
   return (
-    <section className="container px-0 my-12 relative min-h-[31rem] bg-[#01080D]">
+    <section className="container px-0 my-12 relative min-h-[31rem] bg-[#01080D] drop-shadow-2xl">
       <Image src={bg} alt="" fill className="object-contain object-bottom  max-lg:hidden" placeholder="blur"/>
       <Image src={bgWeb} alt=""  className="object-cover  lg:hidden w-full" placeholder="blur"/>
       <div className="absolute lg:top-8 top-[35%] right-[50%] translate-x-[50%] bg-gray-100 px-8 py-2 max-w-sm w-full rounded-2xl ">
