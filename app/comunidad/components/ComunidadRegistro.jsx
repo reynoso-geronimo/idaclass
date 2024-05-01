c435d633dcd611e242457ea0fe39a32f58864e7b
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-01 19:04:42 -0300

    ocultar formulario de registro si el usuario esta registrado

--- app/comunidad/components/ComunidadRegistro.jsx
+++ app/comunidad/components/ComunidadRegistro.jsx
@@ -1,17 +1,37 @@
+"use client"
 import Image from "next/image";
 import bg from "../../../public/comunidad/ImagendefondoFormulario.png";
 import bgWeb from "../../../public/comunidad/ImagendefondoFormularioWeb.png";
 import RegisterForm from "@/app/signin/_components/registerForm";
+import { useSession } from "next-auth/react";
 
 const ComunidadRegistro = () => {
+  const { data: session } = useSession();
   return (
-    <section className="container px-0 my-12 relative min-h-[31rem] bg-[#01080D] drop-shadow-2xl">
-      <Image src={bg} alt="" fill className="object-contain object-bottom  max-lg:hidden" placeholder="blur"/>
-      <Image src={bgWeb} alt=""  className="object-cover  lg:hidden w-full" placeholder="blur"/>
-      <div className="absolute lg:top-8 top-[35%] right-[50%] translate-x-[50%] bg-gray-100 px-8 py-2 max-w-sm w-full rounded-2xl ">
-      <RegisterForm/>
-      </div>
-    </section>
+    <div>
+      {session && session.user ? (
+        ""
+      ) : (
+        <section className="container px-0 my-12 relative min-h-[31rem] bg-[#01080D] drop-shadow-2xl">
+          <Image
+            src={bg}
+            alt=""
+            fill
+            className="object-contain object-bottom  max-lg:hidden"
+            placeholder="blur"
+          />
+          <Image
+            src={bgWeb}
+            alt=""
+            className="object-cover  lg:hidden w-full"
+            placeholder="blur"
+          />
+          <div className="absolute lg:top-8 top-[35%] right-[50%] translate-x-[50%] bg-gray-100 px-8 py-2 max-w-sm w-full rounded-2xl ">
+            <RegisterForm />
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
