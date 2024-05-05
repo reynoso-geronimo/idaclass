e9cdbe853fb726730bde4f27539a01bae8958e89
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-04 23:48:45 -0300

    fixes

--- app/cursos-formacion/[nombre]/_components/cursoFormacionRequisitos.jsx
+++ app/cursos-formacion/[nombre]/_components/cursoFormacionRequisitos.jsx
@@ -4,12 +4,10 @@ const CursoFormacionRequisitos = () => {
   return (
     <section className="flex flex-col lg:flex-row justify-center gap-4 container">
       <article className="w-full rounded-2xl overflow-hidden border-idaclass4 border-2 ">
-        <TituloSeccion
-          className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-idaclass to-idaclass4`}
-        >
+        <TituloSeccion  className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-[#0A456C] to-[#0088CA]`}>
           Requisitos
         </TituloSeccion>
-        <ul className="mx-8 list-outside list-disc flex-1 flex flex-col justify-evenly py-4">
+        <ul className="text-sm mx-8 list-inside list-disc flex-1 flex flex-col justify-evenly py-4">
           <li>Mayor de 18 años o 16 con autorizacion de mayor responsable</li>
           <li>Completar formulario de inscripción</li>
           <li>Apto médico (solo modalidad presencial).</li>
@@ -21,12 +19,10 @@ const CursoFormacionRequisitos = () => {
         </ul>
       </article>
       <article className="w-full rounded-2xl overflow-hidden  border-idaclass4 border-2 ">
-        <TituloSeccion
-          className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-idaclass to-idaclass4`}
-        >
-          A quien va dirigido
+      <TituloSeccion  className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-[#0A456C] to-[#0088CA]`}>
+          ¿A quién va dirigido?
         </TituloSeccion>
-        <ul className="mx-8 list-outside list-disc flex-1 flex flex-col justify-evenly py-4">
+        <ul className="text-sm mx-8 list-inside list-disc flex-1 flex flex-col justify-evenly py-4">
           <li>Público en general, sin conocimientos previos.</li>
           <li>Estudiantes de carreras afines.</li>
           <li>
@@ -36,15 +32,13 @@ const CursoFormacionRequisitos = () => {
       </article>
 
       <article className="w-full rounded-2xl overflow-hidden  border-idaclass4 border-2 ">
-        <TituloSeccion
-          className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-idaclass to-idaclass4`}
-        >
+        <TituloSeccion  className={`py-1 my-0 text-2xl lg:text-2xl text-white bg-gradient-to-b from-[#0A456C] to-[#0088CA]`}>
           Potencia tu empleabilidad
         </TituloSeccion>
         <h3 className="text-center py-4 font-bold">
           Accede a recursos y herramientas para tu exito
         </h3>
-        <ul className="mx-8 list-outside list-disc flex-1 flex flex-col justify-evenly py-4">
+        <ul className="text-sm mx-8 list-inside list-disc flex-1 flex flex-col justify-evenly ">
           <li>Bolsa de trabajo</li>
           <li>Pasantias</li>
           <li>Red de Networking</li>
