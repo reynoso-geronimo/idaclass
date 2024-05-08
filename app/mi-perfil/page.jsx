d81d4aa1abbd6310a75039ddd6d09a1ebd9700ac
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-08 18:07:51 -0300

    probando correcion para que no tire error de build

--- app/mi-perfil/page.jsx
+++ app/mi-perfil/page.jsx
@@ -2,14 +2,14 @@
 import { useSession } from "next-auth/react";
 const Page = () => {
   const { data: session } = useSession();
-  const entries = Object.entries(session.user);
-  console.log(session.user);
-  return (
+  const entries = Object.entries(session?.user) || [];
+  
+{session && session.user?  (
     <div className="container">
       {entries.map((data,i) => (
         <p key={i}>{data}</p>
       ))}
     </div>
-  );
+  ):"no user"}
 };
 export default Page;
