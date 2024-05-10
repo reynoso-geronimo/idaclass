b33fd2f72ca38c2451f0449c82ac4b532098ddb6
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-10 19:38:58 -0300

    fix

--- app/cursos-formacion/[nombre]/_components/cursoFormacionObjetivos.jsx
+++ app/cursos-formacion/[nombre]/_components/cursoFormacionObjetivos.jsx
@@ -3,7 +3,21 @@ import Image from "next/image";
 import React from "react";
 import bgWeb from "../../../../public/cursosFormacion/objetivosBgWeb.png";
 import bgMobile from "../../../../public/cursosFormacion/objetivosBgMobile.png";
-const CursoFormacionObjetivos = ({nombre, ojetivo_titulo_uno, ojetivo_texto_uno, ojetivo_titulo_dos, ojetivo_texto_dos, ojetivo_titulo_tres, ojetivo_texto_tres , perfil_titulo_uno, perfil_texto_uno, perfil_titulo_dos, perfil_texto_dos, perfil_titulo_tres, perfil_texto_tres}) => {
+const CursoFormacionObjetivos = ({
+  nombre,
+  ojetivo_titulo_uno,
+  ojetivo_texto_uno,
+  ojetivo_titulo_dos,
+  ojetivo_texto_dos,
+  ojetivo_titulo_tres,
+  ojetivo_texto_tres,
+  perfil_titulo_uno,
+  perfil_texto_uno,
+  perfil_titulo_dos,
+  perfil_texto_dos,
+  perfil_titulo_tres,
+  perfil_texto_tres,
+}) => {
   return (
     <section className="">
       <div className="container flex flex-col lg:flex-row lg:pt-8 relative">
@@ -26,7 +40,7 @@ const CursoFormacionObjetivos = ({nombre, ojetivo_titulo_uno, ojetivo_texto_uno,
           alt=""
           className="w-full object-contain -translate-y-6 lg:w-1/3 max-lg:hidden"
         />
-        <article className="container text-white pb-8 px-4">
+        <article className="container text-white pb-8 px-4 max-lg:mt-24">
           <h3
             className={
               "max-lg:text-left text-4xl font-extrabold text-transparent bg-gradient-to-b from-idaclass3 to-idaclass4 bg-clip-text"
@@ -34,18 +48,9 @@ const CursoFormacionObjetivos = ({nombre, ojetivo_titulo_uno, ojetivo_texto_uno,
           >
             Objetivos <br className="lg:hidden" /> del Curso
           </h3>
-          <Objetivo
-            titulo={ojetivo_titulo_uno}
-            texto={ojetivo_texto_uno}
-          />
-          <Objetivo
-            titulo={ojetivo_titulo_dos}
-            texto={ojetivo_texto_dos}
-          />
-          <Objetivo
-            titulo={ojetivo_titulo_tres}
-            texto={ojetivo_texto_tres}
-          />
+          <Objetivo titulo={ojetivo_titulo_uno} texto={ojetivo_texto_uno} />
+          <Objetivo titulo={ojetivo_titulo_dos} texto={ojetivo_texto_dos} />
+          <Objetivo titulo={ojetivo_titulo_tres} texto={ojetivo_texto_tres} />
         </article>
         <article className="container text-white pb-8 px-4">
           <h3
