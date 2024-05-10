9e61e972f9efc9d27d3755b300bd93aa2396eed3
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-10 00:39:14 -0300

    sekeleton loader

--- app/mi-perfil/page.jsx
+++ app/mi-perfil/page.jsx
@@ -1,19 +1,36 @@
 "use client";
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Avatar } from "@radix-ui/react-avatar";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+
 const Page = () => {
   const { data: session } = useSession();
 
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    session && session.user & setUser((session.user));
-    console.log(user);
-  }, [session]);
   
-   return(
+
+  return (
     <div className="container">
-        {session && session.user ? ( user.name) : ("no user")}
+      {session && session.user ? (
+        <div className="flex items-center space-x-4">
+          <Avatar className="h-12 w-12 rounded-full" >
+            <AvatarImage src={``} alt="imagen de perfil" />
+            <AvatarFallback>Ida</AvatarFallback>
+          </Avatar>
+          <div className="space-y-2">
+            <p className="h-4 w-[250px]">{session.user.name}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+      )}
     </div>
-   )
+  );
 };
 export default Page;
