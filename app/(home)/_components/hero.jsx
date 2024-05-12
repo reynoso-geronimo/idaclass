4d2e6613f7b26963ccd6ac473c22a56daa875227
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-11 23:27:50 -0300

    fix

--- app/(home)/_components/hero.jsx
+++ app/(home)/_components/hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
       <div className="hidden lg:flex  mb-4 gap-4 container px-0 overflow-hidden">
         <div className="w-2/5 flex flex-col justify-around items-start mx-16 pt-16 ">
           <h2 className="relative w-full text-4xl lg:text-5xl xl:text-6xl font-extrabold mb-4 tracking-tighter animate-in slide-in-from-right fade-in-25 duration-1000 ">
-            <span className="text-white px-4 h-[68px] text-center w-fit flex flex-col overflow-hidden absolute -top-[2px] bg-idaclass4 rounded-3xl">
+            <span className="text-white px-4 h-[55px] xl:h-[68px] text-center w-fit flex flex-col overflow-hidden absolute -top-[2px] bg-idaclass4 rounded-3xl">
               <span className="inline-block animate-text-animation py-[0.15rem] h-[2.81rem] mb-[2.81rem]">
                 Estudia
               </span>
