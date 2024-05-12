09c6405c1868370df6d21a41e5d78c2f838e3a41
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-12 19:18:29 -0300

    cambios en el skeleton loader

--- app/mi-perfil/page.jsx
+++ app/mi-perfil/page.jsx
@@ -28,12 +28,9 @@ const Page = () => {
           <TituloSeccion>MIS CURSOS</TituloSeccion>
         </div>
       ) : (
-        <div className="flex items-center space-x-4">
-          <Skeleton className="h-12 w-12 rounded-full" />
-          <div className="space-y-2">
-            <Skeleton className="h-4 w-[250px]" />
-            <Skeleton className="h-4 w-[200px]" />
-          </div>
+        <div className="flex flex-col items-center  space-y-4 container">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
         </div>
       )}
     </main>
