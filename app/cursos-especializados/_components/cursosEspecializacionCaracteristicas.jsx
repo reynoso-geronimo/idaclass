0b6f02b0a24dcca0b00c0ad19e6e01807e8f0fc8
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-05-15 21:25:37 -0300

    cabmios de texto y headers

--- app/cursos-especializados/_components/cursosEspecializacionCaracteristicas.jsx
+++ app/cursos-especializados/_components/cursosEspecializacionCaracteristicas.jsx
@@ -7,49 +7,49 @@ const caracteristicas = [
     img: "/cursosEspecializacion/Modulos.svg",
     titulo: "Módulos de Aprendizaje",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Los cursos están divididos en secciones con diferentes enfoques del tema principal. Cada una trata un aspecto específico para facilitar el aprendizaje.",
   },
   {
     img: "/cursosEspecializacion/Instructivos.svg",
     titulo: "Videos Instructivos",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Encontrarás videos dinámicos y entretenidos que explicarán los conceptos de forma visual y simple..",
   },
   {
     img: "/cursosEspecializacion/Material.svg",
     titulo: "Material de Lectura",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Tendrás material detallado para complementar los módulos de aprendizaje y obtener una comprensión profunda del tema, a tu propio ritmo.",
   },
   {
     img: "/cursosEspecializacion/Actividades.svg",
     titulo: "Actividades Prácticas",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Ejercicios interactivos, proyectos prácticos o casos de estudio que te permitirán aplicar lo que han aprendido en situaciones reales.",
   },
   {
     img: "/cursosEspecializacion/Evaluaciones.svg",
     titulo: "Evaluaciónes y Exámenes",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Están diseñados para medir la comprensión del material y proporcionarte retroalimentación útil para mejorar tu aprendizaje.",
   },
   {
     img: "/cursosEspecializacion/Recursos.svg",
     titulo: "Recursos Adicionales",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Tendrás disponibles recursos adicionales para enriquecer tu experiencia. Encontrarás libros recomendados, conferencias grabadas, artículos, etc.",
   },
   {
     img: "/cursosEspecializacion/Acceso.svg",
     titulo: "Acceso Ilimitado",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Tendrás acceso de por vida. Podrás hacerlo a tu ritmo y volver a revisar el material del curso todas las veces que lo necesites.",
   },
   {
     img: "/cursosEspecializacion/Certificacion.svg",
     titulo: "Certificación",
     texto:
-      " Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
+      "Podrás validar tus estudios con una certificación reconocida por IdaClass, con autenticación QR exclusiva..",
   },
 ];
 const OPTIONS = { align: "center" };
@@ -66,7 +66,9 @@ const CursosEspecializacionCaracteristicas = () => {
       </div>
       <TituloSeccion className={"lg:my-12"}>
         Características{" "}
-        <span className="text-transparent bg-gradient-to-b from-idaclass3 to-idaclass4 bg-clip-text max-lg:block">On Demand</span>
+        <span className="text-transparent bg-gradient-to-b from-idaclass3 to-idaclass4 bg-clip-text max-lg:block">
+          On Demand
+        </span>
       </TituloSeccion>
       <div className="grid grid-cols-4 gap-x-6 gap-y-6 max-lg:hidden">
         {caracteristicas.map(caracteristica => (
@@ -82,10 +84,10 @@ export const Caracteristica = ({
   titulo = "Titulo",
   texto = "Los cursos se dividen en módulos o secciones que cubean diferentes aspectos del tema principal.",
 }) => (
-  <article className="mx-4 h-[250px] container py-3 w-[280px] lg:w-full bg-gray-100 rounded-3xl text-center flex flex-col justify-evenly items-center gap-3">
-    <Image src={img} alt={titulo} width={64} height={64} />
+  <article className="mx-4 max-lg:h-[250px] container py-3 w-[280px] lg:w-full bg-gray-100 rounded-3xl text-center flex flex-col justify-start items-center gap-3">
+    <Image src={img} alt={titulo} width={64} height={64}/>
     <h3 className="text-idaclass text-xl font-bold">{titulo}</h3>
-    <p className="text-sm text-pretty">{texto}</p>
+    <p className="text-xs lg:text-sm text-balance my-auto">{texto}</p>
   </article>
 );
 
