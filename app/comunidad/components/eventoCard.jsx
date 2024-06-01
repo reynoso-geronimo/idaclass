263581ac143b0b64df9813566edec17ff8bbed91
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-06-01 10:42:09 -0300

    experimentando con una imagen de fallback

--- app/comunidad/components/eventoCard.jsx
+++ app/comunidad/components/eventoCard.jsx
@@ -1,20 +1,32 @@
+"use client";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { ArrowRight, CalendarDaysIcon, Clock } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
 
 const EventoCard = ({ evento }) => {
-  console.log(evento);
+  
+  const [imgError, setImgError] = useState(false)
+
   return (
     <article className="w-full lg:w-1/2  rounded-3xl overflow-hidden text-white mx-auto flex flex-col">
       <div className="w-full mx-auto relative flex flex-col items-start justify-between gap-8 p-4 flex-1 ">
         <Image
+          loader={({ src }) => src}
           src={"/"}
           alt="evento"
           fill
+          onError={(e) => {
+            e.target.id = "./assets/logoIdaClassAlt2.svg";
+            e.target.srcset ="./assets/logoIdaClassAlt2.svg";
+            setImgError(true)
+           
+        }}
           quality={100}
-          className="object-cover -z-10 bg-black"
+          className={`${imgError?"object-scale-down object-right":"object-cover"} -z-10 bg-black`}
         />
         <h2 className="text-4xl font-black mt-8">{evento?.clasificacion}</h2>
         <div className="max-w-[50%]">
