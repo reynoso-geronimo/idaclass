4b0724e6abb470042e6323f4771935e2c54077f7
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-06-30 17:37:57 -0300

    fix styles

--- app/comunidad/evento/[id]/components/Oradores.jsx
+++ app/comunidad/evento/[id]/components/Oradores.jsx
@@ -7,7 +7,7 @@ const Oradores = ({ oradores }) => {
       <TituloSeccion className={"mb-4"}>
         ¿Quiénes dan este Webinar?
       </TituloSeccion>
-      <h2 className="text-center">
+      <h2 className="text-center container">
         Nuestros profesionales con mejor calificación del sector fitness de
         Latinoamérica
       </h2>
@@ -24,9 +24,9 @@ export default Oradores;
 const Orador = ({ orador }) => {
   console.log("orador", orador);
   return (
-    <article className="bg-idaclass5 flex flex-col lg:flex-row p-4 gap-4 rounded-xl">
-      <div className="bg-white w-full h-36 lg:w-52 lg:h-72 shrink-0 rounded-xl my-auto flex items-center justify-center p-4">
-        <Avatar className="lg:w-full lg:h-auto w-auto h-full">
+    <article className="bg-idaclass5 flex flex-col justify-start xl:flex-row p-4 rounded-xl gap-4">
+      <div className="bg-white w-full h-36 xl:w-52 xl:h-72 shrink-0 rounded-xl mt-0 flex items-center justify-center p-4 xl:my-auto">
+        <Avatar className="xl:w-full xl:h-auto w-auto h-full">
           <AvatarImage
             src={`/speakers/${orador.nombre}.png`}
             alt={`${orador.nombre}`}
@@ -34,10 +34,10 @@ const Orador = ({ orador }) => {
           <AvatarFallback>{orador.nombre}</AvatarFallback>
         </Avatar>
       </div>
-      <div className="flex flex-col py-4">
+      <div className="flex flex-col">
         <h3 className="text-idaclass3 font-bold text-2xl">{orador.nombre}</h3>
         <h2 className="text-orange-500 font-bold text-lg">{orador.titulo}</h2>
-        <p className="text-white my-auto">
+        <p className="text-white mt-4">
           {orador.bio && orador.bio?.length > 0 && orador.bio}
         </p>
       </div>
