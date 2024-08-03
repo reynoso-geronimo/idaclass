d5c6761cc364b50ed7114cb1bfbf3ee9fcf475f2
reynoso.geronimo <reynoso.geronimo@gmail.com>
2024-08-03 14:14:56 -0300

    text change

--- app/(home)/_components/certificacion.jsx
+++ app/(home)/_components/certificacion.jsx
@@ -18,13 +18,10 @@ const partners = [
 const Certificacion = () => {
   return (
     <section className="my-12 relative">
-      <TituloSeccion className={"mb-0"}>
-        Obtén la mejor certificación
-      </TituloSeccion>
+      <TituloSeccion className={"mb-0"}>Obtén la mejor certificación</TituloSeccion>
       <h2 className="text-lg container text-center text-balance my-4">
-        Somos la primera academia digital con estándar de formación y los
-        pioneros en contar con normas de código y ética profesional,
-        garantizándote una formación de calidad.
+        Somos la primera academia digital con estándar de formación y los pioneros en contar con normas de código y
+        ética profesional, garantizándote una formación de calidad.
       </h2>
       <div className="container w-full px-0 relative text-white min-h-[310px] lg:min-h-[600px]">
         <Image src={cert1} alt="certificado" className="object-cover" fill />
@@ -47,13 +44,10 @@ const Certificacion = () => {
         </div> */}
         <div className="absolute w-full top-0 right-0 flex justify-end text-white ">
           <div className="flex flex-col justify-center max-lg:hidden">
-            <h5 className="text-2xl text-idaclass3 font-semibold">
-              Certificación universitaria internacional
-            </h5>
+            <h5 className="text-2xl text-idaclass3 font-semibold">Certificación universitaria internacional</h5>
             <p className="max-w-sm">
-              Al finalizar los cursos de formación, podrás acceder a la
-              titulación de extensión universitaria de IUYMCA, con aval
-              internacional.
+              Al finalizar los cursos de formación, podrás acceder a la titulación de extensión universitaria de IUYMCA,
+              con aval internacional.
             </p>
           </div>
           <Image
@@ -65,13 +59,10 @@ const Certificacion = () => {
         </div>
         <div className="absolute bottom-32 w-[420px] right-0 z-10 flex items-center flex-col px-4 mr-8 max-lg:hidden">
           {/* bottom-12 con las imagenes de la universidad */}
-          <h5 className="text-2xl text-idaclass3 font-semibold">
-            Doble titulación
-          </h5>
+          <h5 className="text-2xl text-idaclass3 font-semibold">Doble titulación</h5>
           <p className="">
-            Además, podrás validar tus estudios en nuestra academia con el
-            certificado de IdaClass, con autenticación QR exclusiva, y los 4
-            certificados correspondientes a las especializaciones.
+            Además, podrás validar tus estudios en nuestra academia con el certificado de IdaClass, con autenticación QR
+            exclusiva, y los 4 certificados correspondientes a las especializaciones.
           </p>
           {/*   <h5 className="text-2xl text-idaclass3 font-semibold mt-12 mb-4">
             Nos avalan
@@ -92,28 +83,19 @@ const Certificacion = () => {
       </div>
 
       <div className="container lg:hidden mt-4 text-xs space-y-2">
-        <h5 className="text-2xl text-idaclass3 font-semibold text-center">
-          Certificación universitaria internacional
-        </h5>
+        <h5 className="text-2xl text-idaclass3 font-semibold text-center">Certificación universitaria internacional</h5>
         <p className="text-center">
-          Al finalizar los cursos de formación, podrás acceder a la titulación
-          de extensión universitaria de IUYMCA, con aval internacional.
+          Al finalizar los cursos de formación, podrás acceder a la titulación de extensión universitaria de IUYMCA, con
+          aval internacional.
         </p>
-        <h5 className="text-2xl text-idaclass3 font-semibold text-center">
-          Doble titulación
-        </h5>
+        <h5 className="text-2xl text-idaclass3 font-semibold text-center">Doble titulación</h5>
         <p className="text-center">
-          Además, podrás validar tus estudios en nuestra academia con el
-          certificado de IdaClass, con autenticación QR exclusiva, y los 4
-          certificados correspondientes a las especializaciones.
+          Además, podrás validar tus estudios en nuestra academia con el certificado de IdaClass, con autenticación QR
+          exclusiva, y los 4 certificados correspondientes a las especializaciones.
         </p>
 
         <div className="relative w-full">
-          <Image
-            src={linea}
-            alt="linea"
-            className="absolute -right-10 -bottom-8 scale-150"
-          />
+          <Image src={linea} alt="linea" className="absolute -right-10 -bottom-8 scale-150" />
         </div>
       </div>
     </section>
